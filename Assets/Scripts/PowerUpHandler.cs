27412f9aa2ded803e2f21b191075b5bb072dd19c
Jorikos <26204@ma-web.nl>
2024-02-27 10:49:15 +0100

    Test toegevoegd
    Een kleine test om de functionaliteit van de powerup collision te laten zien

--- Assets/Scripts/PowerUpHandler.cs
+++ Assets/Scripts/PowerUpHandler.cs
@@ -4,11 +4,15 @@ using UnityEngine;
 
 public class PowerUpHandler : MonoBehaviour
 {
+    private PowerUp _powerUp;
+
     private void OnCollisionEnter(Collision collision)
     {
-        switch (collision.gameObject.tag)
+        _powerUp = collision.gameObject.GetComponent<PowerUp>();
+        switch (_powerUp.name)
         {
             case "ExtraHeight": //Testnaam, voeg hier de veranderingen voor de Player Character toe
+                Debug.Log("Extra height power up");
                 return;
         }
     }
