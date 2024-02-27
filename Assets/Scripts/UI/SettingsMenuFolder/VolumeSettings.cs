bee13d90b8143d87a884bf808d52a44299a495b8
rickdohmen <70955143+rickdohmen@users.noreply.github.com>
2024-02-27 11:13:29 +0100

    added scripts main and settings menu
    kan nu in de main menu de knoppen gebruiken om naar andere scenes te switchen,
    voor de settings een volume slider toegevoegd eneen script om de value aan te passen en op te slaan

---
+++ Assets/Scripts/UI/SettingsMenuFolder/VolumeSettings.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VolumeSettings : MonoBehaviour
+{
+    [SerializeField] private Slider volumeSetting;
+    [SerializeField] private Text volumeTextUI;
+
+    private void Start()
+    {
+        volumeSetting.onValueChanged.AddListener(SaveVolume);
+        LoadVolume();
+    }
+
+    // deze functie zorgt er voor dat de value dat verandered is met de slider word opgeslagen in een playerpref
+    //zit nog een error in de ToString
+    public void VolumeSliderController() => volumeTextUI.text = volumeTextUI.ToString("100.0");
+
+    private void SaveVolume(float volume)
+    {
+        float _volumeValue = volumeSetting.value;
+        PlayerPrefs.SetFloat("VolumeValue", _volumeValue);
+        LoadVolume();
+    }
+
+    //deze functie roept de opgslagen value op om te gebruiken voor de volume in de game
+    private void LoadVolume()
+    {
+        var _volumeValue = PlayerPrefs.GetFloat("VolumeValue");
+        volumeSetting.value = _volumeValue;
+        AudioListener.volume = _volumeValue;
+    }
+}
