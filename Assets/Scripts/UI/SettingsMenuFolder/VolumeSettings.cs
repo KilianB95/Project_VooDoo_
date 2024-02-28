d360f74c762b5bb234381dd8fa12277e4d1978ca
rickdohmen <rickdohmen2003@gmail.com>
2024-02-28 13:23:08 +0100

    error fixed in volumesttings
    error in de ToString op line 18 werkt nu zoals het hoort,
    
    moet alleen nog UI text toevoegen en dan is de volume slider af

--- Assets/Scripts/UI/SettingsMenuFolder/VolumeSettings.cs
+++ Assets/Scripts/UI/SettingsMenuFolder/VolumeSettings.cs
@@ -3,32 +3,33 @@ using UnityEngine.UI;
 
 public class VolumeSettings : MonoBehaviour
 {
-    [SerializeField] private Slider volumeSetting;
-    [SerializeField] private Text volumeTextUI;
+    [SerializeField] private Slider _volumeSetting;
+    [SerializeField] private Text _volumeTextUI;
 
     private void Start()
     {
-        volumeSetting.onValueChanged.AddListener(SaveVolume);
+        _volumeSetting.onValueChanged.AddListener(SaveVolume);
         LoadVolume();
     }
 
     // deze functie zorgt er voor dat de value dat verandered is met de slider word opgeslagen in een playerpref
     //zit nog een error in de ToString
     //script is nog niet gelinked
-    public void VolumeSliderController() => volumeTextUI.text = volumeTextUI.ToString("100.0");
+    public void VolumeSliderController(float volume) => _volumeTextUI.text = volume.ToString("100.0");
 
     private void SaveVolume(float volume)
     {
-        float _volumeValue = volumeSetting.value;
-        PlayerPrefs.SetFloat("VolumeValue", _volumeValue);
+        float volumeValue = _volumeSetting.value;
+        PlayerPrefs.SetFloat("VolumeValue", volumeValue);
         LoadVolume();
     }
 
     //deze functie roept de opgslagen value op om te gebruiken voor de volume in de game
     private void LoadVolume()
     {
-        var _volumeValue = PlayerPrefs.GetFloat("VolumeValue");
-        volumeSetting.value = _volumeValue;
-        AudioListener.volume = _volumeValue;
+        var volumeValue = PlayerPrefs.GetFloat("VolumeValue");
+        _volumeSetting.value = volumeValue;
+        AudioListener.volume = volumeValue;
     }
 }
+
