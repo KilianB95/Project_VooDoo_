d677ab616e5c4d4a21dc891d76ebcc97bb5e882e
Jorikos <26204@ma-web.nl>
2024-02-29 13:11:08 +0100

    Folderstructuur opschoning, max jumpforce check, powerup reverse toegevoegd
    Functionaliteit voor het terugdraaien van Power-Ups is toegevoegd, jumpforce heeft nu een maximale waarde ipv elke keer bij elkaar toegevoegd te worden en de folderstructuur is opgeschoond

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,13 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(PowerUpHandler))]
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private Rigidbody _playerRB;
+
     [SerializeField] private Vector3 _jumpForce;
+    private Vector3 _jumpForceMax;
     private bool _isGrounded = true;
 
-    // Update is called once per frame
+    private PowerUpHandler _powerupHandler;
+
+    private void Awake()
+    {
+        _powerupHandler = this.gameObject.GetComponent<PowerUpHandler>();
+        _jumpForceMax = _jumpForce * _powerupHandler.GetJumpMultiplier();
+    }
+
     void FixedUpdate()
     {
         if (Input.GetKey(KeyCode.Space) && _isGrounded)
@@ -35,5 +45,10 @@ public class PlayerMovement : MonoBehaviour
     public void SetJumpForce(Vector3 tempForce)
     {
         _jumpForce = tempForce;
+
+        if (_jumpForce.y > _jumpForceMax.y) //Zet een limiet aan jumpforce
+        {
+            _jumpForce.y = _jumpForceMax.y;
+        }
     }
 }
--- Assets/Scripts/PowerUpHandler.cs
+++ Assets/Scripts/PowerUpHandler.cs
@@ -1,16 +1,16 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PowerUpHandler : MonoBehaviour
 {
-    //PowerUp Types maken, haal uit PowerUp.cs, zet dan _powerUpType naar het juiste type en dan kan je ReversePowerUp() maken.
-    //Als het spawnalgoritme het toelaat kan evt een UnityEvent deze waardes meegegeven worden.
     [SerializeField] private PlayerMovement _playerMovement;
     [SerializeField] private int _jumpMulitplier;
-    [SerializeField] private float _powerupDuration;
+
     private PowerUp _powerUp;
+    private string _powerupTag;
+    private float _powerupDuration;
+    private bool _powerupActive = false;
 
     private void Awake()
     {
@@ -19,9 +19,9 @@ public class PowerUpHandler : MonoBehaviour
 
     private void Update()
     {
-        if (!_powerUp && Time.time >= _powerupDuration)
+        if (Time.time >= _powerupDuration && _powerupActive)
         {
-            //Zet hier ReversePowerUp()
+            ReversePowerUp();
         }
     }
 
@@ -30,20 +30,36 @@ public class PowerUpHandler : MonoBehaviour
         if (collision.gameObject.name.Contains("Powerup")) //Dit moet vervangen worden met een check in de object pooling list
         {
             _powerUp = collision.gameObject.GetComponent<PowerUp>(); //Wanneer dit een powerup is, haal dan het PowerUp component op
-            switch (collision.gameObject.tag)
+            _powerupTag = _powerUp.tag;
+            _powerupActive = true;
+            switch (_powerupTag)
             {
                 case "HeightPowerup":
                     _playerMovement.SetJumpForce(_playerMovement.GetJumpForce() * _jumpMulitplier);
                     ManagePowerUpDuration(); //Zet automatisch de juiste duration in _powerupDuration
                     return;
             }
-
-            _powerUp = null;
         }
     }
 
     private void ManagePowerUpDuration()
     {
         _powerupDuration = Time.time + _powerUp.GetDuration();
+    }
+
+    private void ReversePowerUp()
+    {
+        _powerupActive = false;
+        switch (_powerupTag)
+        {
+            case "HeightPowerup":
+                _playerMovement.SetJumpForce(_playerMovement.GetJumpForce() / _jumpMulitplier);
+                return;
+        }
+    }
+
+    public int GetJumpMultiplier()
+    {
+        return _jumpMulitplier;
     }
 }
