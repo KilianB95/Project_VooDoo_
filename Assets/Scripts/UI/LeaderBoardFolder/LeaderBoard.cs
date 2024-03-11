031be05f26639324c9c0e68823ec3543a4118663
rickdohmen <70955143+rickdohmen@users.noreply.github.com>
2024-03-11 15:31:09 +0100

    Update LeaderBoard.cs
    bezig met er voor zorgen dat als er een highscore is gezet het word opgeslagen in de game

--- Assets/Scripts/UI/LeaderBoardFolder/LeaderBoard.cs
+++ Assets/Scripts/UI/LeaderBoardFolder/LeaderBoard.cs
@@ -2,17 +2,53 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LeaderBoard : MonoBehaviour
+public static class LeaderBoard
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    public const int EntryCount = 10;
+
+    public struct ScoreEntry 
+    { 
+        public string name; 
+        public int Score; 
+
+        public ScoreEntry(string name, int score)
+        {
+            this.name = name;
+            this.Score = score;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private static List<ScoreEntry> _s_Entries;
+
+    private static List<ScoreEntry> Entries 
+    {  
+        get 
+        { 
+            if( _s_Entries == null)
+            {
+                _s_Entries = new List<ScoreEntry>();
+                LoadScores();
+            }      
+        } 
+    }
+
+    private const string PlayerPrefsBaseKey = "leaderBoard";
+
+    private static void SortScores()
     {
-        
+        _s_Entries.Sort((a, b) => b.Score.CompareTo(a.Score));
+    }
+
+    private static void LoadScores()
+    {
+        _s_Entries.Clear();
+
+        for(int i = 0; i < EntryCount; i++)
+        {
+            ScoreEntry _entry;
+            _entry.name = PlayerPrefs.GetString(PlayerPrefsBaseKey + "[" + i + "].name", "");
+            _entry.score = PlayerPrefs.GetInt(PlayerPrefsBaseKey + "[" + i + "].score", 0);
+            _s_Entries.Add(_entry);
+        }
     }
 }
